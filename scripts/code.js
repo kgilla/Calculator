eb9996de08078356c5d7ab43acfb7e0d34c0568a
kgilla <gilmorekenneth@gmail.com>
2020-02-19 18:38:24 -0500

    most script is functioning

--- scripts/code.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelectorAll('.regButton');
-button.forEach((button) => {
-    button.addEventListener('click'. yell, event);
-});
-
-
-function yell () {
-    alert('hello');
-}
